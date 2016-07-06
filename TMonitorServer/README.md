0cc7abc56d96f4817b64894e2f9a1c480debba27
tilliwilli_office <boniface.ru@gmail.com>
2016-07-06 16:24:06 +0400

    Статистика показывается списками

--- TMonitorServer/README.md
+++ TMonitorServer/README.md
@@ -1,8 +1,19 @@
 ﻿# TMonitorServer
-
+HOWTO
+1. git clone https://github.com/TilliWilli5/TMonitorServer.git
+2. cd TMonitorServer/TMonitorServer
+3. npm install
+4. Edit file _Static/JS/devmonitor.js line var sse = new EventSource("http://127.0.0.1/devmonitor/messages"); replace ip address to real server address
 
 TODO
 
 1. Affix with SIgnout option
 2. При новой авторизации инфа о старой сессии должна удаляться из баззы
 3. Сейчас не работает usid per session
+4. Попробовать добавить еще одну инсталяцию в telemetry.json
+5. При нажатии enter в последнем поле ввода на странице аутентификации необходимо делать валидацию
+
+DONE
+1. Кнопка Logout делает своё дело
+2. Можно выбирать дату с помощью datepicker. Дефолтная дата выбрана сегодняшней
+3. Показывается статистика в виде списков
--- TMonitorServer/_Static/JS/dashboard.js
+++ TMonitorServer/_Static/JS/dashboard.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
     $("#installationsScreen").hide();
     $("#statsScreen").hide();
     //Инициализируем виджет с календарями
-    $('#datetimepicker1').datetimepicker({language: 'ru', pickTime: false}).on("change", ShowStats);
+    $('#datetimepicker1').datetimepicker({language: 'ru', pickTime: false, defaultDate:(new Date())}).on("change", ShowStats);
     //VisProjectsScreen(data.telemetryConf);
     VisDashboardHeader(data.telemetryConf);
 });
@@ -36,7 +36,7 @@ function VisDashboardHeader(pProjectsInfo)
             for(var iX=0; iX<instaCount; ++iX)
             {
                 instaName = pProject.installations[iX].label;
-                instaList += `<li><a href="javascript:void(0);" onclick="GotoStatsScreen(this);">${instaName}</a></li>`;
+                instaList += `<li><a href="javascript:void(0);" onclick="GotoStatsScreen(this);ShowStats();">${instaName}</a></li>`;
             }
             instaList += `</ul>`;
         projectTemplate += instaList;
@@ -61,6 +61,7 @@ function GotoStatsScreen(pTarget)
     window.current.installation = currentInstallationIndex;
     var buttonGroups = [];
     var curTelemetryStructure = data.telemetryConf[current.project].installations[current.installation].telemetry;
+    window.current.telemetryStructure = curTelemetryStructure;
     for(var iX=0; iX<curTelemetryStructure.length; ++iX)
     {
         if(buttonGroups.indexOf(curTelemetryStructure[iX].buttonGroup) === -1)
@@ -73,5 +74,73 @@ function GotoStatsScreen(pTarget)
 function ShowStats()
 {
     console.log($('#datetimepicker1 input').val());
+    $("#statsHere").empty();
+    current.buttonGroup = {};
+    //iX - ButtonGroup selection
+    for(var iX=0; iX<current.bgs.length; ++iX)
+    {
+        current.buttonGroup[current.bgs[iX]] = [];
+        var panelBG  = `<div class="panel panel-info">
+        <div class="panel-heading">${current.bgs[iX]}</div>
+        <div class="panel-body">`;
+
+            var info = '<div class="list-group">';
+            //iY - current.telemetryStructure selection
+            for(var iY=0; iY<current.telemetryStructure.length; ++iY)
+            {
+                //Проверяем на принадлежность к данной ButtonGroup
+                if(current.telemetryStructure[iY].buttonGroup !== current.bgs[iX])
+                    continue;
+                current.buttonGroup[current.bgs[iX]][iY] = {name:current.telemetryStructure[iY].label, value:0};
+                // current.buttonGroup[current.bgs[iX]].name = current.telemetryStructure[iY].label;
+                // current.buttonGroup[current.bgs[iX]].value = 0;
+                for(var iZ=0; iZ<data.telemetryData.length; ++iZ)
+                {
+                    
+                    if(data.telemetryConf[current.project].ticket === data.telemetryData[iZ]["projects.ticket"])
+                        if(data.telemetryConf[current.project].installations[current.installation].token === data.telemetryData[iZ]["tm.token"])
+                        {
+                            var theDate = $('#datetimepicker1 input').val();
+                            theDate = theDate.split(".").reverse().join("-");
+                            if(theDate === data.telemetryData[iZ]["tm.dates"])
+                                if(current.telemetryStructure[iY].telemetrySignal === data.telemetryData[iZ]["tm.point"])
+                                {
+                                    current.buttonGroup[current.bgs[iX]][iY].value = data.telemetryData[iZ]["tm.hits"];
+                                    break;
+                                }
+                        }
+                }
+                info += `<a href="#" class="list-group-item">${current.buttonGroup[current.bgs[iX]][iY].name}: ${current.buttonGroup[current.bgs[iX]][iY].value}</a>`;
+            }
+            info += '</div>';
+
+            // var iY=0;
+            // while(iY<data.telemetryData.length)
+            // {
+            //     if(data.telemetryConf[current.project].ticket === data.telemetryData[iY]["projects.ticket"])
+            //     {
+            //         if(data.telemetryConf[current.project].installations[current.installation].token === data.telemetryData[iY]["tm.token"])
+            //         {
+            //             var buttonName = 
+            //             info += `<a href="#" class="list-group-item active">First item</a>`;
+
+            //         }
+
+            //     }
+            // }
+
+            panelBG += info;
+        panelBG += '</div>\
+        </div>';
+        
+        $("#statsHere").append(panelBG);
+    }
     
+
+};
+function Logout()
+{
+    document.cookie = `userSessionID=; expires=${new Date(0)}; path=/`;
+    window.location.href = "/";
+    console.log("Logout");
 };
